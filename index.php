c63979d612ed3dfe41ccb094c3b78d68f4b5d21e
dassonjoy <dassonjoy551@gmail.com>
2021-12-27 02:34:41 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Original Dev CodySeller But Customize Heavily
+*/
+header('Access-Control-Allow-Origin: *');
+
+
+require_once(__DIR__.'/includes/config.php');
+
+$app->run();
+
+
+
---
+++ lol.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <style media="screen">
+    *{
+      padding: 0;
+      margin:0;
+      box-sizing: bor
+
+    }
+      body{
+background: #010101;
+display: flex;
+justify-content: center;
+
+align-items: center;
+height: 100vh;
+      }
+    </style>
+  </head>
+  <body>
+<img src='https://telegra.ph/file/84378222d1908baaf5e93.png' alt='error'>
+  </body>
+</html>
