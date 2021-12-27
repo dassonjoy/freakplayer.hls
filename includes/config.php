20bb35f2830de8eb6b681d53bdb4eb1f6392783b
dassonjoy <81907377+dassonjoy@users.noreply.github.com>
2021-12-27 22:57:56 +0600

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -16,7 +16,7 @@ define('DEBUG', false);
  */
 define('PROOT', '');
 
-define('HLS_API_SECRET_KEY', 'ddWA2322!35e7');
+define('HLS_API_SECRET_KEY', 'freakplayer-ml');
 
 $allowed_domains = ['localhost'];
 
